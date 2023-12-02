e42af8e21d9d26857ea55630860c60dc68e04fdf
Shubhash Singh <subhashsingh2059@gmail.com>
2023-12-02 11:53:31 +0530

    Exceptaion Handling

---
+++ Exception Handling/Custom_Exceptation.java
@@ -0,0 +1,9 @@
+/*
+ * Java provides us facility to create our own exceptation which are basically derived class of exception class
+
+ * Creating our own exception is known as a custom exception or user defined exception
+ 
+ * Java custom exception are used to customize the exceptaions according to user needs
+ * 
+ */
+
